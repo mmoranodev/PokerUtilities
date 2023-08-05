323a33aacfb9badceff54333b6f8dcb63fef1a0c
Matt Dev <mmoranodev@gmail.com>
2023-08-05 21:44:04 +0000

    Add driver for testing

---
+++ src/Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+    public static void main(String[] args){
+        assertTrue(true);
+
+    }
+
+    public static void assertTrue(boolean tf){
+        System.out.println(String.valueOf(tf));
+    }
+}
